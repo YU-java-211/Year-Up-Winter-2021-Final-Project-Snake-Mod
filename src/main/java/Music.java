393f46fbb2296a52d4d2bfaf1fa767bac3a90601
alejandroch495 <alejandroch495@gmail.com>
2021-03-09 23:02:34 -0800

    Create Music.java
    -Used to create any now sound objects :speaker:
    
    -To use you must create a String   variable  with a unique name and store sound/music file path here
    
    String exampleSoundPath = "src/resources/example.wav";
    
    -Next you need a new "Music" object
    
    Music example = new Music();
    
    Sound file requirements :bangbang: :
    File format: .wav
    Sample rate: 48k
    Bit depth: 16 Bit integer

---
+++ src/main/java/Music.java
@@ -0,0 +1,27 @@
+
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Music {
+    void playSound(String filePath) {
+        try {
+            File mpath = new File(filePath);
+
+            if (mpath.exists()) {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(mpath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+
+            } else {
+                System.out.println(" Can not find file! ");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
