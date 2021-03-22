9a3296d5521a05fadbc36bb268686184d0248645
anisahabdu <67612553+anisahabdu@users.noreply.github.com>
2021-03-21 19:24:12 -0700

    Adds NewLevel class, Score class, and changes to Snake class.

--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -1,18 +1,52 @@
 package main.java;
 
+import java.util.Scanner;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Snake extends JFrame {
 
     public Snake() {
         
-        initUI();
+    	firstLevelUI();
     }
     
-    private void initUI() {
-        
-        add(new Board());
+    
+    
+    private void firstLevelUI() {
+    	String points = "";
+    	try {
+    	File file = new File("pointsLivesDate.txt");
+    
+	    Scanner scanner = new Scanner(file);
+    	
+
+	    while(scanner.hasNextLine()) {
+	    	points = scanner.nextLine();
+	    }
+
+	    } catch (FileNotFoundException e) {
+	        System.out.println("An error occurred.File not found.");
+	        e.printStackTrace();
+	        points = "";
+	        
+	        add(new Board());
+            
+	        setResizable(false);
+	        pack();
+	        
+	        setTitle("Snake");
+	        setLocationRelativeTo(null);
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      }
+    	
+    	String[] parsePoints = points.split("|");
+    	int previousPoints = Integer.parseInt(parsePoints[0]);
+    	int previousLives = Integer.parseInt(parsePoints[1]);
+	    
+        add(new NewLevel( previousPoints, previousLives));
                
         setResizable(false);
         pack();
@@ -21,6 +55,8 @@ public class Snake extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+    
+    
     
 
     public static void main(String[] args) {
